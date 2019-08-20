06b846cc9913d5af64c1532a9b1e1d104968ea8c
beta0221 <beta0221@gmail.com>
2019-08-20 21:53:22 +0800

    更新帳戶資料 api

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,7 @@ Route::group([
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
     Route::post('myAccount','MemberController@myAccount');
+    Route::post('updateAccount','MemberController@updateAccount');
 
 });
 
