b8bcb62f7ced577ba3b51d242b1da5e604ea2f3b
Movark <movark@Movarkde-MacBook-Pro.local>
2019-08-24 21:53:35 +0800

    .

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -73,7 +73,7 @@ class MemberController extends Controller
 
         try {
             
-            $users = User::whereBetween('id',[176,250])->get();
+            $users = User::whereBetween('id',[251,400])->get();
 
             foreach($users as $user){
 
