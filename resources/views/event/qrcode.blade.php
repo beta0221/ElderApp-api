cc39e751c4d0341db54ee53cce3735c3f956843a
beta0221 <beta0221@gmail.com>
2019-11-19 22:21:12 +0800

    qrcode arrival code

--- resources/views/event/qrcode.blade.php
+++ resources/views/event/qrcode.blade.php
@@ -9,16 +9,25 @@
 </style>
 </head>
 <body>
+<div style="height:600px">
+    <h1 style="margin-top:40px">領取活動獎勵：{{$event->title}}</h1>
+    <h3 style="margin-bottom:80px;">請掃描下方 QR Code 領取活動獎勵。</h3>
+    <div id="reward"></div>
+</div>
+<div style="height:600px">
+    <h1 style="margin-top:40px">活動簽到：{{$event->title}}</h1>
+    <h3 style="margin-bottom:80px;">請掃描下方 QR Code 進行簽到。</h3>
+    <div id="arrive"></div>
+</div>
 
-<h1 style="margin-top:40px">領取活動獎勵：{{$event->title}}</h1>
-<h3 style="margin-bottom:80px;">請掃描下方 QR Code 領取活動獎勵。</h3>
-<div id="output"></div>
+
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
 <script type="text/javascript" src="/js/jquery.qrcode.min.js"></script>
 <script>
 jQuery(function(){
-	jQuery('#output').qrcode("{{$event->slug}}");
+    jQuery('#reward').qrcode("reward,{{$event->slug}}");
+    jQuery('#arrive').qrcode("arrive,{{$event->slug}}");
 })
 </script>
 
