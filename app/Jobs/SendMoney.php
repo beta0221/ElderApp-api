0164d7ecb6f3bd6b2decc5d1642eb0840583032f
beta0221 <beta0221@gmail.com>
2020-05-27 23:57:20 +0800

    .

--- app/Jobs/SendMoney.php
+++ app/Jobs/SendMoney.php
@@ -39,27 +39,35 @@ class SendMoney implements ShouldQueue
     {
 
         sleep(3);
-        $result = Transaction::where('user_id',$this->user->id)->where('event','大天使鼠年紅包')->first();
+
+        $result = Transaction::where('user_id',$this->user->id)->get();
         if(!$result){
             return;
         }
         
-        // if($trans = Transaction::where('user_id',$this->user->id)->where('event','鼠年紅包')->first()){
-        //     $this->user->updateWallet(false,$trans->amount);
-        //     $trans->delete();
-        // }
+        $hasSend = false;
+        foreach ($result as $r) {
+            if($r->event == '鼠年紅包' || $r->event == '鼠年紅包(補發)' || $r->event == '小天使鼠年紅包' || $r->event == '大天使鼠年紅包'){
+                $hasSend = true;
+            }
+        }
+        
+
+        if(!$hasSend){
+            $this->user->updateWallet(true,$this->amount);
+
+            $tran_id = time() . rand(10,99);
+            Transaction::create([
+                'tran_id'=>$tran_id,
+                'user_id'=>$this->user->id,
+                'event' =>$this->event,
+                'amount'=>$this->amount,
+                'target_id'=>0,
+                'give_take'=>true,
+            ]);
+        }
 
 
-        $this->user->updateWallet(true,$this->amount);
-
-        $tran_id = time() . rand(10,99);
-        Transaction::create([
-            'tran_id'=>$tran_id,
-            'user_id'=>$this->user->id,
-            'event' =>$this->event,
-            'amount'=>$this->amount,
-            'target_id'=>0,
-            'give_take'=>true,
-        ]);
+        
     }
 }
