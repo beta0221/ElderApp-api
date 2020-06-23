14eb8431e39528831cd0cf80a1c8a7d73169f5bf
Micky <sdps40105@gmail.com>
2020-06-24 00:07:45 +0800

    new table

--- resources/views/order/list.blade.php
+++ resources/views/order/list.blade.php
@@ -10,20 +10,44 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            <div class="offset-sm-6">
-
-            </div>
-            <div class="col-sm-3">
-                <span>123</span>
-            </div>
-
-
-
+        <div class="align-items-center">
+            <div class="col-sm-12">
+                <h1>我的訂單</h1>
+                <hr>
+            </div>    
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>日期</th>
+                        <th>訂單編號</th>
+                        <th>產品</th>
+                        <th>價格</th>
+                        <th>數量</th>
+                        <th>紅利折扣</th>
+                        <th>總價</th>
+                        <th>付款方式</th>
+                        <th>付款狀態</th>
+                        <th>出貨狀態</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>2020-06-23</td>
+                        <td>123456</td>
+                    </tr>
+                    <tr>
+                        <td>2020-02-21</td>
+                        <td>12344567</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table> 
             
-            <div class="col-sm-3">
-                <span>456</span>
-            </div>
+            
+           
         </div>
     </div>
 @endsection
