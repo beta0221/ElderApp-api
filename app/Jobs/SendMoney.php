da0a412a488bbc1d631e4e46de9589ba3699b18b
Movark <>
2020-07-01 15:46:04 +0800

    hotfix 發放 旅遊獎勵

--- app/Jobs/SendMoney.php
+++ app/Jobs/SendMoney.php
@@ -40,42 +40,17 @@ class SendMoney implements ShouldQueue
 
         sleep(3);
 
-        $result = Transaction::where('user_id',$this->user->id)->get();
-        if(!$result){
-            return;
-        }
-        
-        $hasSend = false;
-        foreach ($result as $r) {
-            if($r->event == '鼠年紅包' || $r->event == '鼠年紅包(補發)' || $r->event == '小天使鼠年紅包' || $r->event == '大天使鼠年紅包'){
-                $hasSend = true;
-            }
-        }
-        
+        $this->user->updateWallet(true,$this->amount);
 
-        if(!$hasSend){
-
-            if($this->user->org_rank == 2){
-                $this->event = '小天使鼠年紅包(補發)';
-                $this->amount = 300;
-            }else if($this->user->org_rank == 3){
-                $this->event = '大天使鼠年紅包(補發)';
-                $this->amount = 600;
-            }
-
-            $this->user->updateWallet(true,$this->amount);
-
-            $tran_id = time() . rand(10,99);
-            Transaction::create([
-                'tran_id'=>$tran_id,
-                'user_id'=>$this->user->id,
-                'event' =>$this->event,
-                'amount'=>$this->amount,
-                'target_id'=>0,
-                'give_take'=>true,
-            ]);
-        }
-
+        $tran_id = time() . rand(10,99);
+        Transaction::create([
+            'tran_id'=>$tran_id,
+            'user_id'=>$this->user->id,
+            'event' =>$this->event,
+            'amount'=>$this->amount,
+            'target_id'=>0,
+            'give_take'=>true,
+        ]);
 
         
     }
