edfaf016748985651cd399f2e72d695106db587d
Movark <>
2020-07-07 15:47:39 +0800

    .

--- resources/views/order/list.blade.php
+++ resources/views/order/list.blade.php
@@ -13,20 +13,28 @@
 
         <div class="row align-items-center mt-2">
             <div class="col-sm-12">
-                <h3>我的訂單</h3>
-                
+                <h4>我的訂單</h4>
             </div>    
         </div>
 
         <div class="row">
             <div class="col-sm-12">
-                <table>
+                <table style="width: 100%">
                     <tr>
                         <th>日期</th>
                         <th>訂單編號</th>
                         <th>產品</th>
                         <th></th>
                     </tr>
+                    
+                    @if(count($orderList)==0)
+                    <tr style="height: 160px">
+                        <td class="mt-4 mb-4" colspan="4" style="text-align: center">
+                            <h4>無訂單記錄</h4>
+                        </td>
+                    </tr>
+                    @endif
+
                     @foreach ($orderList as $order)
                     <tr>
                         <td>{{$order->created_at}}</td>
