85fe55a2bb2a1e15af93ca040e5e24ebeec5d667
beta0221 <beta0221@gmail.com>
2020-11-27 01:33:25 +0800

    fix migration problem

--- database/migrations/2020_11_27_013249_create_location_manager_table.php
+++ database/migrations/2020_11_27_013249_create_location_manager_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLocationClerkTable extends Migration
+class CreateLocationManagerTable extends Migration
 {
     /**
      * Run the migrations.
