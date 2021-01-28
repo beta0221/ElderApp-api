e5f17663a4ba762b8ebcda92c49bca10d6cdc1d8
beta0221 <beta0221@gmail.com>
2021-01-28 10:26:03 +0800

    custome confirm dialog

--- resources/views/event/days_qrcode.blade.php
+++ resources/views/event/days_qrcode.blade.php
@@ -6,6 +6,18 @@
     body{
         text-align: center!important;
     }
+    .alert-box{
+        position: fixed;
+        top: 30%;
+        left: 50%;
+        transform: translateX(-50%);
+        background: #fff;
+        border:1px solid gray;
+        border-radius: .3rem;
+        min-width: 240px;
+        width: 80%;
+        padding: 24px;
+    }
 </style>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
@@ -34,12 +46,38 @@
                 </div>
                 
                 @for($x = 1; $x <= $event->days;$x++)
-                    <div onclick="updateCurrentDay({{$x}})" class="btn btn-lg btn-info d-block mb-2 {{($event->current_day>=$x)?'disabled':''}}">第 {{$x}} 堂課</div>
+                    <div onclick="confirmUpdateCurrentDay({{$x}})" class="btn btn-lg btn-info d-block mb-2 {{($event->current_day>=$x)?'disabled':''}}">第 {{$x}} 堂課</div>
                 @endfor
             </div>
         </div>
     </div>
 
+
+    <div class="alert-box confirm" style="display: none">
+        <div>
+            <h1>訊息</h1>
+        </div>
+        <div class="mt-4 mb-4" style="font-size: 32px">
+            <span id="message">訊息</span>
+        </div>
+        <div>
+            <div onclick="cancleDialog()" class="btn btn-secondary btn-lg" style="display:inline-block;width:49%">取消
+            </div><div style="display: inline-block;width:2%">
+            </div><div onclick="updateCurrentDay()" class="btn btn-primary btn-lg" style="display:inline-block;width:49%">確定</div>
+        </div>
+    </div>
+
+    <div class="alert-box my-alert" style="display: none;">
+        <div>
+            <h2>訊息</h2>
+        </div>
+        <div class="mt-4 mb-4" style="font-size: 24px">
+            <span id="message">訊息</span>
+        </div>
+        <div>
+            <div onclick="cancleDialog()" class="btn btn-primary btn-lg" style="display: block">確定</div>
+        </div>
+    </div>
 
     
 
@@ -53,17 +91,32 @@ const token = `Bearer ${localStorage.getItem('token')}`;
 
 jQuery(function(){
     jQuery('#reward').qrcode("reward,{{$event->slug}}");
-})
+});
 
-function updateCurrentDay(day){
-    if(parseInt(day) <= maxDays){ return; }
-    // if(!confirm('確認是否開啟第'+day+'堂課')){ return; }
+var updateDay = null;
+function confirmUpdateCurrentDay(day){
+    updateDay = day;
+    $('.confirm #message').html('確認是否開啟第 ' + day + ' 堂課');
+    $('.confirm').show();
+};
 
+function cancleDialog(){
+    $('.alert-box').hide();
+};
+
+function customeAlert(message){
+    $('.my-alert #message').html(message);
+    $('.my-alert').show();
+};
+
+function updateCurrentDay(){
+    if(parseInt(updateDay) <= maxDays){ return; }
+    cancleDialog();
     $.ajax({
         type: "POST",
         url: "/api/updateEventCurrentDay/"+ slug,
         data: {
-            'upToDay':day,
+            'upToDay':updateDay,
         },
         dataType: "json",
         headers:{
@@ -73,16 +126,16 @@ function updateCurrentDay(day){
             if(response.s==1){
                 window.location.reload();
             }else{
-                alert(response.m);
+                customeAlert(response.m);
             }
         },
         error:function(error){
-            alert(error);
+            customeAlert(error);
         }
     });
 
 
-}
+};
 
 
 
