bf06080e03c6bcb29aa27d5d9203879516141195
beta0221 <beta0221@gmail.com>
2021-01-31 19:38:53 +0800

    view adjustment

--- resources/views/event/days_qrcode.blade.php
+++ resources/views/event/days_qrcode.blade.php
@@ -23,7 +23,7 @@
 </head>
 <body>
 
-    <div style="height:500px">
+    <div style="height:720px">
         <h1 style="margin-top:40px">領取活動獎勵：{{$event->title}}</h1>
         <h3 style="margin-bottom:80px;">請掃描下方 QR Code 領取活動獎勵。</h3>
         <div id="reward"></div>
