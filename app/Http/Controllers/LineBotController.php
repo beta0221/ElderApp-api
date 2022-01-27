6bc8ae5a90d03e82a3cc9264bd6c206dda7954b0
beta0221 <beta0221@gmail.com>
2022-01-27 17:23:10 +0800

    verify signature test

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -8,6 +8,24 @@ class LineBotController extends Controller
 {
     
     public function webhook(Request $request){
+
+
+
+        $channelSecret = '845775fbea34b0c480bdc4b84c47d078'; // Channel secret string
+        $httpRequestBody = $request->getContent(); // Request body string
+        $hash = hash_hmac('sha256', $httpRequestBody, $channelSecret, true);
+        $signature = base64_encode($hash);
+
+        $x_line_signature = $request->header('x-line-signature');
+
+        if($signature != $x_line_signature){
+            return response('error',500);
+        }
+
+
+
+
+
         return response('success',200);
     }
 
